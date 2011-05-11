13f16cbd3d91e7fbe515bc87a75b2c91826403a6
Leonid Volnitsky <Leonid@Volnitsky.com>
2011-05-11 10:32:16 +0300

    -- a.cc

--- 2011-qualification/a.cc
+++ 2011-qualification/a.cc
@@ -2,12 +2,12 @@
 int main() {
 	int	T(in);
 	for (int t=1;  t<=T;  t++) {
-		int	N(in);
 		vint	pos	{1,1};
 		vint	time	{0,0};
 		int	now	{0};
 
-		for (int n=0;  n<N*2;  n+=2) {
+		int	N(in);
+		REP (N) {
 			int  r= char(in)!='O';
 			int  p(in);
 			int  need_time = abs(pos[r]-p);
